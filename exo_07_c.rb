80e8d40c83de616ff0a5c667add8a894d0825e34
EliaTREILHOU <67633877+EliaTREILHOU@users.noreply.github.com>
2020-07-07 21:07:28 +0200

    Delete exo_07_c.rb

--- exo_07_c.rb
+++
@@ -1,4 +0,0 @@
-user_name = gets.chomp
-puts user_name
-
-# Les différences des 3 programmes est : le 1er il demande le prenom et fait un echo dessus / le 2ème il met une > devant l'édriture et 3ème il ne peut pas l'executer car pas de puts ou print
